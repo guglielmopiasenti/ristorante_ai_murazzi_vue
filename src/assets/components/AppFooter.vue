553cec5bd7979390ac6b29fa3e1ff02df075b624
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-23 18:10:48 +0200

    footer first draft

---
+++ src/assets/components/AppFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <body>
+
+    <div class="bg-black text-white p-8 container mx-auto grid grid-cols-1 justify-center gap-y-12">
+
+      <!-- Navigation links -->
+      <div class="flex space-x-8 justify-center">
+        <a href="#" class="hover:text-gray-400">About</a>
+        <a href="#" class="hover:text-gray-400">Blog</a>
+        <a href="#" class="hover:text-gray-400">Jobs</a>
+        <a href="#" class="hover:text-gray-400">Press</a>
+        <a href="#" class="hover:text-gray-400">Accessibility</a>
+        <a href="#" class="hover:text-gray-400">Partners</a>
+      </div>
+
+      <!-- Social icons -->
+      <div class="flex space-x-12 mt-4 md:mt-0 justify-center">
+        <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-youtube"></i></a>
+      </div>
+      <div class="text-center text-gray-500">
+        <p>&copy; 2023 Ai Murazzi, All rights reserved.</p>
+      </div>
+    </div>
+
+
+  </body>
+</template>
