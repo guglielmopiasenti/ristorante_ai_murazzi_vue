e1d4ee7ebf7bc79b70e0f4942526e43a97bed3ae
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-27 22:12:10 +0200

    first draft

---
+++ src/assets/pages/WorkPage.vue
@@ -0,0 +1,8 @@
+<script>
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+
+</script>
