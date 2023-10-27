656f1a9473d3283f5199d5e4ba2ee9df76878a90
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-27 22:10:21 +0200

    footer final draft

--- src/assets/components/AppFooter.vue
+++ src/assets/components/AppFooter.vue
@@ -3,14 +3,21 @@
 
     <!-- Navigation links -->
     <div class="flex flex-wrap tracking-tight lg:space-x-8 justify-center gap-5">
-      <a href="#" class="hover:text-gray-400">Su di noi</a>
-      <a href="#" class="hover:text-gray-400">Lavoro</a>
-      <a href="#" class="hover:text-gray-400">Il ristorante</a>
+      <RouterLink class=" hover:text-gray-400" :to="{ name: 'about-us' }">
+        Su di noi
+      </RouterLink>
+      <RouterLink class=" hover:text-gray-400" :to="{ name: 'work' }">
+        Lavoro
+      </RouterLink>
+      <RouterLink class=" hover:text-gray-400" :to="{ name: 'restaurant' }">
+        Il Ristorante
+      </RouterLink>
     </div>
 
     <!-- Social icons -->
     <div class="flex space-x-12 mt-4 md:mt-0 justify-center">
-      <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-facebook-f"></i></a>
+      <a href="https://www.facebook.com/RistoranteAiMurazzi/?locale=it_IT" class="text-xl hover:text-gray-400"><i
+          class="fab fa-facebook-f"></i></a>
       <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-instagram"></i></a>
       <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-twitter"></i></a>
       <a href="#" class="text-xl hover:text-gray-400"><i class="fab fa-youtube"></i></a>
