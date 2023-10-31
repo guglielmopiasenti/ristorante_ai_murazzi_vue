2acd92d341e313ffb547a4ee5c2fd51012a52c02
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-31 11:16:16 +0100

    about us completed

--- src/assets/pages/AboutUsPage.vue
+++ src/assets/pages/AboutUsPage.vue
@@ -56,7 +56,8 @@
         <div class="m-2 space-y-2">
           <div class="group flex flex-col gap-2 rounded-lg bg-black p-5 text-white" tabindex="1">
             <div class="flex cursor-pointer items-center justify-between">
-              <span class="font-bold"> È possibile pagare con una carta di credito al ristorante Ristorante Ai Murazzi?
+              <span class="font-bold text-xl"> È possibile pagare con una carta di credito al ristorante Ristorante Ai
+                Murazzi?
               </span>
               <img src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
                 class="h-2 w-3 transition-all duration-500 group-focus:-rotate-180" />
@@ -69,7 +70,7 @@
 
           <div class="group flex flex-col gap-2 rounded-lg bg-black p-5 text-white" tabindex="2">
             <div class="flex cursor-pointer items-center justify-between">
-              <span class="font-bold"> Ristorante Ai Murazzi Il ristorante dispone di parcheggio? </span>
+              <span class="font-bold text-xl"> Ristorante Ai Murazzi Il ristorante dispone di parcheggio? </span>
               <img src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
                 class="h-2 w-3 transition-all duration-500 group-focus:-rotate-180" />
             </div>
