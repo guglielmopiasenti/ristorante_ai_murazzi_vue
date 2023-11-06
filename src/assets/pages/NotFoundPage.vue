1b517cb1cf850ad98d23ed20a9a5208f21c86e0d
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-06 08:35:38 +0100

    style completed

--- src/assets/pages/NotFoundPage.vue
+++ src/assets/pages/NotFoundPage.vue
@@ -1,23 +1,24 @@
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full">
-    <body class="h-full">
-    ```
-  -->
-  <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+  <AppHeader />
+  <main class="grid min-h-full place-items-center bg-black px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-      <p class="text-base font-semibold text-indigo-600">404</p>
-      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-      <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+      <p class="text-base font-semibold text-white">404</p>
+      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-5xl">Pagina non trovata</h1>
+      <p class="mt-6 text-base leading-7 text-gray-600">Ci dispiace, ma questa pagina non esiste.</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="#"
-          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go
-          back home</a>
-        <a href="#" class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
+        <RouterLink
+          class="text-lg rounded-full bg-gradient-to-r from-[#3A261E] via-[#734E3B] to-[#D8B58C] px-3.5 py-2.5 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+          :to="{ name: 'home' }">
+          Torna alla Home
+        </RouterLink>
       </div>
     </div>
   </main>
+  <AppFooter />
 </template>
+
+
+<script setup>
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
+</script>
